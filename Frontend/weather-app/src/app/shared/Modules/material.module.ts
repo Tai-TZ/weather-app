96fb6e116fb16eda84f70eff6b2223091db41114
Thanh Tai <n.t.tai435@gmail.com>
2025-08-12 00:04:58 +0700

    Feat: Init base code

---
+++ Frontend/weather-app/src/app/shared/Modules/material.module.ts
@@ -0,0 +1,106 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Form Controls
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+// Navigation
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+
+// Layout
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatStepperModule } from '@angular/material/stepper';
+
+// Buttons & Indicators
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+// Popups & Modals
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+// Data table
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTreeModule } from '@angular/material/tree';
+
+// Date adapter
+import { MatNativeDateModule } from '@angular/material/core';
+
+const MATERIAL_MODULES = [
+    // Form Controls
+    MatAutocompleteModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+
+    // Navigation
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTabsModule,
+
+    // Layout
+    MatCardModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatListModule,
+    MatStepperModule,
+
+    // Buttons & Indicators
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatBadgeModule,
+    MatChipsModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+
+    // Popups & Modals
+    MatDialogModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+
+    // Data table
+    MatPaginatorModule,
+    MatSortModule,
+    MatTableModule,
+    MatTreeModule,
+
+    // Date adapter
+    MatNativeDateModule
+];
+
+@NgModule({
+    imports: [CommonModule, ...MATERIAL_MODULES],
+    exports: [...MATERIAL_MODULES]
+})
+export class MaterialModule { }
